fa7f737ff43633b2503b7ae0902cccb10918b7ee
”riosBlack” <glinhas55@hotmail.com>
2025-11-02 13:45:35 -0300

    FIX: BUG

--- auth.edge.config.ts
+++ auth.edge.config.ts
@@ -10,18 +10,10 @@ export const authEdgeConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
-      const isOnAuth =
-        nextUrl.pathname.startsWith("/login") ||
-        nextUrl.pathname.startsWith("/register");
 
       // Redireciona para login se tentar acessar dashboard sem estar logado
       if (isOnDashboard && !isLoggedIn) {
         return false;
-      }
-
-      // Redireciona para dashboard se já estiver logado e tentar acessar login/register
-      if (isLoggedIn && isOnAuth) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
       }
 
       return true;
